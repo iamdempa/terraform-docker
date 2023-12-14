f1d08b0e9a6c23bd9cef341b1a17e81bf2ce13da
Jananath Banuka Jayarathna <32482769+iamdempa@users.noreply.github.com>
2023-12-14 18:42:00 +0000

    added test file

--- main.tf
+++ main.tf
@@ -1,23 +1,6 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
+resource "null_resource" "example1" {
+  provisioner "local-exec" {
+    command = "open WFH, '>completed.txt' and print WFH scalar localtime"
+    interpreter = ["perl", "-e"]
   }
-}
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
-# Pulls the image
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
-}
-
-# Create a container
-resource "docker_container" "foo" {
-  image = docker_image.ubuntu.image_id
-  name  = "foo"
 }
